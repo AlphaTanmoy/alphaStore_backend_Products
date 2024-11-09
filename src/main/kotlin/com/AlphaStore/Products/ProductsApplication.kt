a23401e7ddbb48ca57427a7673ab3020c910ad45
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:31:31 +0530

    update

--- src/main/kotlin/com/AlphaStore/Products/ProductsApplication.kt
+++ src/main/kotlin/com/AlphaStore/Products/ProductsApplication.kt
@@ -1,13 +1,11 @@
-package com.AlphaStore.Products
+package com.alphaStore.Products
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class ProductsApplication
 
 fun main(args: Array<String>) {
-    runApplication<ProductsApplication>(*args)
+	runApplication<ProductsApplication>(*args)
 }
